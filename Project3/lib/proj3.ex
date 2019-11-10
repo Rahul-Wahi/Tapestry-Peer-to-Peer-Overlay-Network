8ca4284d2ca71f891716161f803e161fed85c499
Wins Goyal <winsgoyal.iitj@gmail.com>
2019-11-09 19:30:24 -0500

    More comments deleted

--- Project3/lib/proj3.ex
+++ Project3/lib/proj3.ex
@@ -8,12 +8,7 @@ defmodule Proj3.Tapestry do
     
     
     {_ ,[noOfNodes, numRequests ],_} = OptionParser.parse(args ,  strict: [n: :integer, n: :integer])
-    #case OptionParser.parse(System.argv() ,  strict: [n: :integer, k: String, o: String]) do
   
-      #{_ ,[noOfNodes, topology , algorihm],_} -> divideArgAndCallFunc(String.to_integer(a),\\b))  ### For Nodes
-      #{_ ,[a,b],_} -> app(String.to_integer(a),String.to_integer(b))
-     # _ -> app(1,2)
-      #end
       if String.to_integer(noOfNodes) > 0 and String.to_integer(numRequests) > 0 do
         noOfNodes = String.to_integer(noOfNodes)
         numRequests = String.to_integer(numRequests)
